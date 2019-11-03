e22a67ba9f11509c1162b263e05831385d7c185e
pstoebenau <pstoebenau@knights.ucf.edu>
2019-11-02 20:32:22 -0400

    Finished problem 2

--- ProgAssign4/stoebenau_p2/src/Application.java
+++ ProgAssign4/stoebenau_p2/src/Application.java
@@ -1,5 +1,13 @@
-public class Application {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Application {
+    public static void main(String[] args) throws IOException {
+        DuplicateCounter dc = new DuplicateCounter();
+        String projectDir = System.getProperty("user.dir");
+        String dataDir = projectDir + "/problem2.txt";
+        String outDir = projectDir + "/unique_word_counts.txt";
+
+        dc.count(dataDir);
+        dc.write(outDir);
     }
 }
--- ProgAssign4/stoebenau_p2/src/DuplicateCounter.java
+++ ProgAssign4/stoebenau_p2/src/DuplicateCounter.java
@@ -1,5 +1,46 @@
-public class DuplicateCounter {
-    public void count(String dataFile) {
+import java.io.*;
+import java.util.*;
 
+public class DuplicateCounter {
+    HashMap<String, Integer> wordCounter = new HashMap<String, Integer>();
+
+    public void count(String dataFile) throws IOException{
+        File file = new File(dataFile);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            String word = scanner.next();
+            addCountToMap(word);
+        }
+
+        scanner.close();
+    }
+
+    public void write(String outFile) throws IOException {
+        File file = new File(outFile);
+        FileWriter fileWriter = new FileWriter(file);
+        String output;
+        StringBuilder outputBuilder = new StringBuilder();
+
+        for (Map.Entry<String, Integer> entry : wordCounter.entrySet()) {
+            String key = entry.getKey();
+            Integer val = entry.getValue();
+            outputBuilder.append(key).append(": ").append(val).append(" occurrences\n");
+        }
+
+        output = outputBuilder.toString();
+
+        fileWriter.write(output);
+        fileWriter.close();
+    }
+
+    private void addCountToMap(String word) {
+        if (!wordCounter.containsKey(word)) {
+            wordCounter.put(word, 1);
+            return;
+        }
+
+        int newCount = wordCounter.get(word) + 1;
+        wordCounter.put(word, newCount);
     }
 }
